4bd3d407593aad8b8af9af94cd013704923761cf
Sameer Garg <garg.sameer@gmail.com>
2012-10-03 17:53:54 +0100

    removing files for puppet

--- manifests/files/websphere/jvm_config.py
+++
@@ -1,6 +0,0 @@
-#This script is responsible for setting up JVM settings
-# in WebSphere - e.g. Heap size
-
-jvm = AdminConfig.list("JavaVirtualMachine").split("\n")[0]
-AdminConfig.modify(jvm, '[[maximumHeapSize 1024]]')
-AdminConfig.save()
